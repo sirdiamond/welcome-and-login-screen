04066cf34f671dff31a0bd19ca792d2c65d70d76
sirdiamond <rodrigo.pnq@gmail.com>
2020-07-10 19:13:40 -0300

    welcome page created

---
+++ lib/screens/signin/sigin.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SiginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+      backgroundColor: Colors.grey[900],
+    );
+  }
+}
---
+++ lib/screens/welcome/welcome.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:welcome/screens/signin/sigin.dart';
+
+class WelcomeSreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: Column(
+        children: [
+          Expanded(
+            flex: 8,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('assets/bakery.jpeg'),
+                fit: BoxFit.cover,
+              )),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.all(21.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  WelcomeText(),
+                  WelcomeButton(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WelcomeText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'BAKING LESSONS',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 27.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          Text(
+            'MASTER THE ART OF BAKING',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w400,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class WelcomeButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RaisedButton.icon(
+            color: Color(0xFFFFBD73),
+            label: Text(
+              'START LEARNING',
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w800),
+            ),
+            icon: Icon(Icons.trending_flat),
+            onPressed: () {
+              _pushToSignIn(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+void _pushToSignIn(BuildContext context) {
+  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+    return SiginScreen();
+  }));
+}
